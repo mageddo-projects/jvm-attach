02f4d775c45010b4d506561959ae96ace57ad4aa
Elvis de Freitas <edigitalb@gmail.com>
2020-06-20 23:06:36 -0300

    setup linux

--- jvm-attach/src/main/java/com/mageddo/platform/Os.java
+++ jvm-attach/src/main/java/com/mageddo/platform/Os.java
@@ -1,21 +1,22 @@
 package com.mageddo.platform;
 
+import java.util.Optional;
+
 public enum Os {
 
-  LINUX(true, "Linux"),
-  OSX(true, "osx"),
-  WINDOWS("windows"),
+  LINUX(true, false, "Linux"),
+  OSX(true, false, "osx"),
+  WINDOWS(false, true,"windows"),
+  WINDOWS_7(false, true, "windows 7"),
   ;
 
   private final String[] codes;
   private boolean posix;
+  private boolean windows;
 
-  Os(String... codes){
-    this(false, codes);
-  }
-
-  Os(boolean posix, String... codes) {
+  Os(boolean posix, boolean windows, String... codes) {
     this.posix = posix;
+    this.windows = windows;
     this.codes = codes;
   }
 
@@ -23,16 +24,34 @@ public enum Os {
     return this.posix;
   }
 
+  public boolean isWindows(){
+    return this.windows;
+  }
+
   public static Os fromOsName() {
     final String osName = System.getProperty("os.name");
+    final Os os = Optional
+      .ofNullable(fromOsName(osName))
+      .orElseGet(() -> fromGenericOsName(osName));
+    if(os == null) {
+      throw new IllegalArgumentException("Operation system not found for code: " + osName);
+    }
+    return os;
+  }
+
+  private static Os fromGenericOsName(String osName) {
+    return fromOsName(osName.split(" ")[0]);
+  }
+
+  private static Os fromOsName(String osName) {
     for (final Os os : values()) {
       for (final String osCode : os.codes) {
-        if(osCode.equals(osName)){
+        if(osCode.equalsIgnoreCase(osName)){
           return os;
         }
       }
     }
-    throw new IllegalArgumentException("Operation system not found for code: " + osName);
+    return null;
   }
 
 }
